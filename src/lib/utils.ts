4530c37d0779708ea4dba4216f6b21bbdd411d02
rRoler <60528736+rRoler@users.noreply.github.com>
2026-07-23 02:56:17 +0200

    change All to Multi in getLocaleName util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -204,7 +204,7 @@ export function sleep(ms: number): Promise<void> {
 }
 
 export function getLocaleName(locale: string): string {
-	if (locale === 'multi') return 'All';
+	if (locale === 'multi') return 'Multi';
 	const langDisplayNames = new Intl.DisplayNames(['en'], { type: 'language' });
 	return langDisplayNames.of(locale) || locale;
 }
